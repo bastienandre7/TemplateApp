962472dcd9e886881d728e76c18e0eccc5f8b1da
Bastien Andre <bastienandre68@gmail.com>
2025-06-09 18:21:17 +0200

    Removed promo banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import BannerCPN from "@/components/Main/BannerCPN";
 import MainContainer from "@/components/Main/MainContainer";
-import PromoBanner from "@/components/PromoBanner";
 
 export default async function Home() {
   const res = await fetch(`${process.env.NEXTAUTH_URL}/api/products`, {
@@ -9,7 +8,6 @@ export default async function Home() {
   const data = await res.json();
   return (
     <div className="min-h-screen bg-white">
-      <PromoBanner />
       <BannerCPN />
       <MainContainer products={data} />
     </div>
